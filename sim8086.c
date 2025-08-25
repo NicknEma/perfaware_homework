c2b8eed99148c6fbd7bab37fd79691e6f3a48b28
NicknEma <emanuele.rovini@gmail.com>
2025-08-25 20:30:14 +0200

    Compute clock estimate for effective address calculation

--- sim8086.c
+++ sim8086.c
@@ -217,6 +217,56 @@ static int count_ones_i8(i8 n) {
 	return ones;
 }
 
+static int clocks_from_effective_address_expression(effective_address_expression *expr) {
+	int register_count = 0;
+	if (expr->Terms[0].Register.Index != Register_None)
+		register_count += 1;
+	if (expr->Terms[1].Register.Index != Register_None)
+		register_count += 1;
+	
+	int displacement = expr->Displacement;
+	
+	int clocks = 0;
+	if (register_count == 0) {
+		clocks = 6;
+	} else if (register_count == 1) {
+		if (displacement == 0)
+			clocks = 5;
+		else
+			clocks = 9;
+	} else {
+		int big = 0;
+		if (expr->Terms[0].Register.Index == Register_bp) {
+			if (expr->Terms[1].Register.Index == Register_di)
+				big = 0;
+			else
+				big = 1;
+		} else {
+			if (expr->Terms[1].Register.Index == Register_si)
+				big = 0;
+			else
+				big = 1;
+		}
+		
+		if (displacement == 0) {
+			if (!big)
+				clocks = 7;
+			else
+				clocks = 8;
+		} else {
+			if (!big)
+				clocks = 11;
+			else
+				clocks = 12;
+		}
+	}
+	
+	if (expr->Flags & Address_ExplicitSegment)
+		clocks += 2;
+	
+	return clocks;
+}
+
 static bool simulate_8086_instruction(instruction instr, u16 *registers, u32 register_count, u8 *memory, u32 memory_size) {
 	bool halt = 0;
 	
