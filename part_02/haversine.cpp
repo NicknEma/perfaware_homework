08b04bc7a40154a66d049da96e535edf7c90ecb9
NicknEma <emanuele.rovini@gmail.com>
2025-08-27 22:34:02 +0200

    Sketch base structure of json parser

---
+++ part_02/haversine.cpp
@@ -0,0 +1,392 @@
+#include <assert.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+
+//
+// Base
+//
+
+typedef  uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef   int8_t  i8;
+typedef  int16_t i16;
+typedef  int32_t i32;
+typedef  int64_t i64;
+
+typedef    float f32;
+typedef   double f64;
+
+#define array_count(a) (sizeof(a)/sizeof((a)[0]))
+
+#define sl_expand_pfirst(s) (s), sizeof(s)
+#define sl_expand_sfirst(s) sizeof(s), (s)
+#define sl(s) make_string(sizeof(s)-1, (u8 *) (s))
+
+#define ss_expand_pfirst(s) (s).data, (s).len
+#define ss_expand_sfirst(s) (s).len, (s).data
+
+struct string {
+	i64 len;
+	u8 *data;
+};
+
+static string make_string(i64 len, u8 *data) {
+	string result = {len, data};
+	return result;
+}
+
+static bool string_equals(string a, string b) {
+	bool result = false;
+	if (a.len == b.len) {
+		result = memcmp(a.data, b.data, sizeof(u8) * a.len) == 0;
+	}
+	return result;
+}
+
+struct Temp_Storage {
+	u8 *ptr;
+	i64 cap;
+	i64 pos;
+};
+
+static Temp_Storage temp_storage = {};
+
+static void init_temp_storage() {
+	temp_storage.cap = 1024*1024*1024;
+	temp_storage.ptr = (u8 *) calloc(1, temp_storage.cap);
+	temp_storage.pos = 0;
+}
+
+static void free_temp_storage() {
+	temp_storage.pos = 0;
+}
+
+static void *temp_push_nozero(i64 size) {
+	void *result = 0;
+	
+	if (temp_storage.pos + size < temp_storage.cap) {
+		result = temp_storage.ptr + temp_storage.pos;
+		temp_storage.pos += size;
+	}
+	
+	assert(result);
+	
+	return result;
+}
+
+static void *temp_push(i64 size) {
+	void *result = temp_push_nozero(size);
+	memset(result, 0, size);
+	
+	return result;
+}
+
+static char *tsprintf(char *fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	
+	size_t needed = vsnprintf(0, 0, fmt, args) + 1;
+	char  *buffer = (char *) temp_push(sizeof(char) * (i64) needed);
+	
+	vsnprintf(buffer, needed, fmt, args);
+	
+	va_end(args);
+	return buffer;
+}
+
+static string read_entire_file(char *name) {
+	string result = {};
+	
+	assert(!"unimplemented"); (void) name;
+	
+	return result;
+}
+
+//
+// Haversine computation
+//
+
+union Point {
+	struct { f64 x, y; };
+	f64 v[2];
+};
+
+union Pair {
+	struct { f64 x0, y0, x1, y1; };
+	struct { Point p0, p1; };
+	Point points[2];
+};
+
+#define TAU 44.0/7.0
+static f64 radians_from_degrees(f64 deg) { return deg * TAU / 360.0; }
+
+static f64 haversine_of_degrees(f64 x0, f64 y0, f64 x1, f64 y1, f64 r) {
+	f64 dY = radians_from_degrees(y1 - y0);
+	f64 dX = radians_from_degrees(x1 - x0);
+	y0 = radians_from_degrees(y0);
+	y1 = radians_from_degrees(y1);
+	
+	f64 sindy2 = sin(dY/2);
+	f64 sindx2 = sin(dX/2);
+	
+	f64 root_term = (sindy2*sindy2) + cos(y0)*cos(y1)*(sindx2*sindx2);
+	f64 result = 2*r*asin(sqrt(root_term));
+	
+	return result;
+}
+
+//
+// Json utils
+//
+
+enum Json_Token_Kind : u32 {
+	Json_Token_None,
+	Json_Token_Invalid,
+	
+	Json_Token_Comma,
+	Json_Token_Colon,
+	Json_Token_Lbrack,
+	Json_Token_Rbrack,
+	Json_Token_Lbrace,
+	Json_Token_Rbrace,
+	Json_Token_String,
+	Json_Token_Number,
+	
+	Json_Token_EOI,
+};
+
+struct Json_Token {
+	Json_Token_Kind kind;
+	string str;
+};
+
+struct Json_Parse_Ctx {
+	string input;
+	i32 input_index;
+	
+	Json_Token curr_token;
+	
+	i32 save_index;
+};
+
+static Json_Token make_json_token(Json_Parse_Ctx *parser);
+
+static Json_Token peek_json_token(Json_Parse_Ctx *parser) {
+	if (parser->curr_token.kind == Json_Token_None) {
+		parser->curr_token = make_json_token(parser);
+	}
+	return parser->curr_token;
+}
+
+static void consume_json_token(Json_Parse_Ctx *parser) {
+	parser->curr_token = make_json_token(parser);
+}
+
+static Json_Token make_json_token(Json_Parse_Ctx *parser) {
+	Json_Token token = {};
+	
+	string input = parser->input;
+	i32 index = parser->input_index;
+	
+	for (int i = index; i < input.len && isspace(input.data[i]); i += 1) {
+		index += 1;
+	}
+	
+	if (index < input.len) {
+		if (token.kind == Json_Token_None && input.data[index] == '"') {
+			i32 start = index;
+			i32 opl = index;
+			
+			for (; index < input.len && input.data[index] != '"'; index += 1) {
+				opl += 1;
+			}
+			
+			token.kind = Json_Token_String;
+			token.str = {opl - start, input.data + start};
+		}
+		
+		if (token.kind == Json_Token_None && isdigit(input.data[index])) {
+			i32 start = index;
+			i32 opl = index;
+			
+			for (; index < input.len && (isdigit(input.data[index]) || input.data[index] == '.'); index += 1) {
+				opl += 1;
+			}
+			
+			token.kind = Json_Token_Number;
+			token.str = {opl - start, input.data + start};
+		}
+		
+		if (token.kind == Json_Token_None) {
+			struct Kind_Char_Pair { Json_Token_Kind kind; char c; };
+			static Kind_Char_Pair token_map[] = {
+				{Json_Token_None, '\0'},
+				{Json_Token_Comma, ','},
+				{Json_Token_Colon, ':'},
+				{Json_Token_Lbrack, '['},
+				{Json_Token_Rbrack, ']'},
+				{Json_Token_Lbrace, '{'},
+				{Json_Token_Rbrace, '}'},
+			};
+			
+			Json_Token_Kind best_match = Json_Token_None;
+			for (int i = 0; i < array_count(token_map); i += 1) {
+				if (input.data[index] == token_map[i].c) {
+					best_match = token_map[i].kind;
+				}
+			}
+			
+			if (best_match != Json_Token_None) {
+				token.kind = best_match;
+				token.str = {1, input.data + index};
+				
+				index += 1;
+			}
+		}
+	} else {
+		token.kind = Json_Token_EOI;
+	}
+	
+	if (token.kind == Json_Token_None) {
+		token.kind = Json_Token_Invalid;
+	}
+	
+	parser->input_index = index;
+	parser->curr_token = token;
+	
+	return token;
+}
+
+//
+// Assuming Json parser
+//
+
+struct Parsed_Pairs {
+	Pair *pairs;
+	i32 pair_count;
+	
+	bool ok;
+};
+
+static Parsed_Pairs parse_json_pairs(char *name) {
+	Parsed_Pairs result = {};
+	result.ok = true;
+	
+	Json_Parse_Ctx parser = {};
+	parser.input = read_entire_file(name);
+	
+	// {
+	Json_Token token = peek_json_token(&parser);
+	if (token.kind == Json_Token_String) {
+		consume_json_token(&parser);
+	} else {
+		result.ok = false;
+	}
+	
+	// "pairs"
+	if (result.ok) {
+		token = peek_json_token(&parser);
+		if (token.kind == Json_Token_String) {
+			string contents = {token.str.len - 2, token.str.data + 1};
+			if (string_equals(contents, sl("pairs"))) {
+				consume_json_token(&parser);
+			} else {
+				result.ok = false;
+			}
+		} else {
+			result.ok = false;
+		}
+	}
+	
+	// :
+	if (result.ok) {
+		token = peek_json_token(&parser);
+		if (token.kind == Json_Token_Colon) {
+			consume_json_token(&parser);
+		} else {
+			result.ok = false;
+		}
+	}
+	
+	// [
+	if (result.ok) {
+		token = peek_json_token(&parser);
+		if (token.kind == Json_Token_Lbrack) {
+			consume_json_token(&parser);
+		} else {
+			result.ok = false;
+		}
+	}
+	
+	//
+	// Loop here...
+	//
+	
+	// ]
+	if (result.ok) {
+		token = peek_json_token(&parser);
+		if (token.kind == Json_Token_Rbrack) {
+			consume_json_token(&parser);
+		} else {
+			result.ok = false;
+		}
+	}
+	
+	// }
+	if (result.ok) {
+		token = peek_json_token(&parser);
+		if (token.kind == Json_Token_Rbrace) {
+			consume_json_token(&parser);
+		} else {
+			result.ok = false;
+		}
+	}
+	
+	if (!result.ok) {
+		fprintf(stderr, "Invalid json\n");
+	}
+	
+	return result;
+}
+
+//
+// Main program
+//
+
+int main(int argc, char **argv) {
+	bool ok = true;
+	
+	char *json_name = 0;
+	bool show_usage = false;
+	
+	if (argc > 1) {
+		json_name = argv[1];
+	} else {
+		show_usage = true;
+	}
+	
+	if (show_usage) {
+		fprintf(stderr, "Usage:\n\t%s [haversine_input.json]\n", argv[0]);
+		ok = false;
+	} else {
+		init_temp_storage();
+		
+		auto parsed = parse_json_pairs(json_name);
+		if (parsed.ok) {
+			
+		} else {
+			ok = false;
+		}
+	}
+	
+	return !ok;
+}
