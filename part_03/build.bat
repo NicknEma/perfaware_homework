03ed6c1edae036788f4bc4327bd0f74f0f3470cd
NicknEma <emanuele.rovini@gmail.com>
2025-09-11 00:52:16 +0200

    Update build.bat

--- part_03/build.bat
+++ part_03/build.bat
@@ -1,5 +1,5 @@
 @echo off
-del *.pdb > NUL 2> NUL
+REM del *.pdb > NUL 2> NUL
 cl %* /nologo /Z7 /W4 /external:anglebrackets /external:W0 /wd4201 /wd4996 /wd4505 /link /incremental:no /opt:ref
 del *.obj > NUL 2> NUL
 del *.ilk > NUL 2> NUL
