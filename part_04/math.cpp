72e2f1fd9bad52d24cffe9ac26c54fefb7a50c00
NicknEma <emanuele.rovini@gmail.com>
2025-09-14 15:26:39 +0200

    Math functions error checker

---
+++ part_04/math.cpp
@@ -0,0 +1,27 @@
+#if _MSC_VER
+#include "intrin.h"
+#else
+#include "x86intrin.h"
+#endif
+
+#define PI64 3.14159265358979323846264338327950288419716939937510582097494459230781640628
+
+static f64 hav_sqrt(f64 x) {
+	f64 y = x;
+	return y;
+}
+
+static f64 hav_asin(f64 x) {
+	f64 y = x;
+	return y;
+}
+
+static f64 hav_sin(f64 x) {
+	f64 y = x;
+	return y;
+}
+
+static f64 hav_cos(f64 x) {
+	f64 y = x;
+	return y;
+}
---
+++ part_04/test_math_functions_main.cpp
@@ -0,0 +1,95 @@
+#include "shared.h"
+
+#include "shared.cpp"
+#include "math.cpp"
+#include "reference_haversine.cpp"
+
+//
+// 1) Make sure the *reference* functions are correct, by comparing them against some
+// hand-picked results
+// 2) Make sure the *custom* implementations are correct *in the range we care about*, by
+// comparing them against the reference ones.
+//
+
+typedef f64 Math_Func(f64);
+
+struct Reference_Answer {
+	f64 x, y;
+};
+
+static void print_spaces(u32 space_count) {
+	for (u32 space_index = 0; space_index < space_count; space_index += 1) {
+		printf(" ");
+	}
+}
+
+static void test_against_hardcoded_values(char *name, Math_Func *func, u32 ref_count, Reference_Answer *refs) {
+	printf("%s:\n", name);
+	for (u32 ref_index = 0; ref_index < ref_count; ref_index += 1) {
+		f64 ref_x = refs[ref_index].x;
+		f64 ref_y = refs[ref_index].y;
+		
+		u32 space_count = printf("f(%+.24f) ", ref_x);
+		printf("= %+.24f [reference]\n", ref_y);
+		
+		f64 got_y = func(ref_x);
+		f64 error = fabs(got_y - ref_y);
+		
+		print_spaces(space_count);
+		printf("= %+.24f (off by %.24f) [%s]\n", got_y, error, name);
+	}
+	printf("\n");
+}
+
+static Reference_Answer sqrt_ref_answers[] = {
+	{0.00, 0.0},
+	{0.25, 0.5},
+	{0.50, 0.7071067811865476},
+	{0.75, 0.8660254037844386},
+	{1.00, 1.0},
+};
+
+static void test_against_reference_implem(char *ref_name, Math_Func *ref_func, char *name, Math_Func *func,
+										  f64 domain_min, f64 domain_max, u32 sample_count = 100000) {
+	
+	printf("%s (comparing against %s):\n", name, ref_name);
+	
+	f64 acc_error = 0;
+	f64 max_error = 0;
+	f64 max_error_at = 0;
+	for (u32 sample_index = 0; sample_index < sample_count; sample_index += 1) {
+		f64 t = (f64)sample_index / (f64)(sample_count - 1);
+		f64 x = (1.0 - t) * domain_min + t * domain_max;
+		
+		f64 ref_y = ref_func(x);
+		f64 got_y = func(x);
+		
+		f64 error = fabs(got_y - ref_y);
+		acc_error += error;
+		if (max_error < error) {
+			max_error = error;
+			max_error_at = x;
+		}
+	}
+	
+	f64 avg_error = sample_count != 0 ? acc_error / sample_count : 0;
+	
+	printf("Max error: %+.24f (found when x = %+.24f)\n", max_error, max_error_at);
+	printf("Avg error: %+.24f\n", avg_error);
+	
+	printf("\n");
+}
+
+int main() {
+	test_against_hardcoded_values("sqrt", sqrt, array_count(sqrt_ref_answers), sqrt_ref_answers);
+	test_against_hardcoded_values("asin", asin, 0, 0);
+	test_against_hardcoded_values("sin",  sin,  0, 0);
+	test_against_hardcoded_values("cos",  cos,  0, 0);
+	
+	test_against_reference_implem("sqrt", sqrt, "hav_sqrt", hav_sqrt,  0.0,       1.0);
+	test_against_reference_implem("asin", asin, "hav_asin", hav_asin,  0.0,       1.0);
+	test_against_reference_implem("sin",  sin,  "hav_sin",  hav_sin,  -PI64,     +PI64);
+	test_against_reference_implem("cos",  cos,  "hav_cos",  hav_cos,  -PI64/2.0, +PI64/2.0);
+	
+	return 0;
+}
